ff850bdcd20292ff32a21a8f739d551190a7d42b
aaaa24 <90201411+aaaa24@users.noreply.github.com>
2026-04-12 13:35:57 +0300

    Динамическое название мессенджера в информации о боте

--- src/messenger_context.py
+++ src/messenger_context.py
@@ -5,3 +5,8 @@ def get_users_table(messenger='telegram'):
 def get_messenger_from_kwargs(kwargs):
     messenger = kwargs.get('messenger') if isinstance(kwargs, dict) else None
     return messenger or 'telegram'
+
+
+def get_nice_name_of_messenger_from_kwargs(kwargs):
+    messenger = get_messenger_from_kwargs(kwargs)
+    return 'Telegram' if messenger == 'telegram' else 'MAX' if messenger == 'max' else messenger
